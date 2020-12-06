35c68446667f1f1b73f92e258a7af1b849e8dcf2
Christine Taylor <CTaylor100@gmail.com>
2020-12-06 11:22:58 -0500

    Fetch to API Working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React from 'react';
 
-function App() {
+const baseURL = 'http://localhost:3000';
+console.log('Current Base URL:', baseURL);
+
+const App = (props) => {
+  const [movies, setMovies] = React.useState([])
+
+  const getMovies = async () => {
+    const res = await fetch(baseURL + '/movies');
+    const data = await res.json();
+    setMovies(data);
+    console.log(movies)
+  }
+
+  React.useEffect(() => { getMovies() }, []);
+
   return (
     <h1>Testing</h1>
   );
